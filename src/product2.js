3c0edd0a935a38fb205c0645005e79222703dc60
Ankesh Kumar <ankesh9053@gmail.com>
2023-01-22 16:48:09 +0530

    lets see

--- src/product2.js
+++ src/product2.js
@@ -49,7 +49,7 @@ function sliderrange(){
 // console.log(input.value)
 
 let bag = [];
-let container = document.querySelector("#container");
+let container = document.querySelector("#product-container");
 fetch("json/women.json")
   .then((res) => {
     return res.json()
@@ -93,7 +93,9 @@ function display(data) {
 
     let button = document.createElement("button")
     button.textContent = "Tap To Add";
-    button.style.backgroundColor = "green";
+    button.style.backgroundColor = "#f15a22";
+    button.style.border="1px solid black";
+    button.style.color="white"
     button.style.borderRadius = "10px"
     button.style.height = "30px"
     button.addEventListener("click", function () {
@@ -175,7 +177,7 @@ function display(data) {
 
    function search(){
     container.innerHTML=null
-    let q = document.querySelector("input").value;
+    let q = document.querySelector(".input").value;
     console.log("working")
     let newdata = bag.filter(function(element){
      return element.brand.toLowerCase().includes(q.toLowerCase())
